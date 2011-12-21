0cf8cdfc9689d715131aa6d98d52cbdc203078c3
wulff <wulff@ratatosk.net>
2011-12-21 14:39:42 +0100

    added file mode

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -3,6 +3,7 @@ class iptables {
     '/etc/iptables.up.rules':
       source => 'puppet:///modules/iptables/iptables.up.rules';
     '/etc/network/if-pre-up.d/iptables':
-      source => 'puppet:///modules/iptables/iptables';
+      source => 'puppet:///modules/iptables/iptables',
+      mode => 0755;
   }
 }
